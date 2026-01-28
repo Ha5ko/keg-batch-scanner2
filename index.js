473b44f9812b16432a8e1d5abb3f480df26ec9d1
Ha5ko <shantu.hasko@ab-inbev.com>
2026-01-28 13:12:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-/**
- * Keg Batch Scanner
- * React Native Entry Point
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-// Register the main component
 AppRegistry.registerComponent(appName, () => App);
